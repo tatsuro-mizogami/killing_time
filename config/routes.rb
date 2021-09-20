fa76805952364e5c2a7a0f649e72bf528b09d995
tatsuro <mizogamitatsurou@mizokamitatsurounoiMac.local>
2021-09-20 12:28:35 +0900

    Revert "ページの修正"
    This reverts commit 3d3710c35831a0d652263e008b512d11f4f75a53.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'games#index'
   get 'games/index2' => 'games#index2'
-  get 'games/index3' => 'games#index3'
-  resources :games, only: [:index]
+  resources :games, only: [:index, :new, :create]
 end
